a3ba9950a7086953470bded26d28327478cda864
klaudek02 <klaudekmen2@gmail.com>
2018-10-25 19:50:49 +0200

    Added login via google account, test login page, user data view, + security, login dependencies

--- src/main/java/com/bookweb/BookService/Controller/ServiceController.java
+++ src/main/java/com/bookweb/BookService/Controller/ServiceController.java
@@ -1,4 +1,18 @@
 package com.bookweb.BookService.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@Controller
 public class ServiceController {
+    @RequestMapping("/user")
+    @ResponseBody
+    public Principal user(Principal principal)
+    {
+        return principal;
+    }
 }

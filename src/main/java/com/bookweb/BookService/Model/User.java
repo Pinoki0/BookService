dbbac2c71e1d06fdc6c8b43050d5aa599fb88d37
Pinoki0 <erykdrzewiecki@vp.pl>
2018-10-26 21:48:42 +0200

    #User

--- src/main/java/com/bookweb/BookService/Model/User.java
+++ src/main/java/com/bookweb/BookService/Model/User.java
@@ -1,8 +1,63 @@
 package com.bookweb.BookService.Model;
 
+
+import org.springframework.data.annotation.Id;
+
 public class User {
-    int b=0;
-    int c=2;
-    int d=6;
-    int e=4;
+    @Id
+    private String id;
+
+    private String name;
+    private String picture;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+    public User() {}
+
+    public User(String picture, String email, String name) {
+        this.name=name;
+        this.email=email;
+        this.picture=picture;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s,email='%s', name='%s',  picture='%s' ]",
+                id, email, name, picture);
+    }
 }
